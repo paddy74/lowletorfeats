36127a33e43b561442c02550e4996d96364f8e1e
Patrick Cox <pxcox003@odu.edu>
2019-04-03 18:47:37 -0400

    Create lowletorfeats.hpp

---
+++ include/lowletorfeats/lowletorfeats.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "base/llfstd.hpp"
+
+
+namespace lowletorfeats
+{
+
+base::DocFeatureMap collectDocFeatures(
+    base::TermFrequencyMap docTermFrequencies
+)
+{
+
+}
+
+
+base::DocFeatureMap collectDocFeatures(
+    base::TermFrequencyMap docTermFrequencies,
+    base::TermFrequencyMap queryTermFrequencies
+)
+{
+
+}
+
+}
