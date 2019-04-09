6c949cb13cdb4e8514e97323fefcf53a70c500c2
Patrick Cox <pxcox003@odu.edu>
2019-04-09 17:05:26 -0400

    Create enum for all feature names

--- include/lowletorfeats/base/stdllf.hpp
+++ include/lowletorfeats/base/stdllf.hpp
@@ -1,11 +1,39 @@
 #pragma once
 
 #include <map>
+#include <unordered_map>
 #include <vector>
 
 
 namespace lowletorfeats::base
 {
-    typedef std::map<std::string, uint> TermFrequencyMap;  // Map of term frequency
-    typedef std::map<std::string, double> DocFeatureMap;  // Features for a given document
+    typedef std::unordered_map<std::string, uint> TermFrequencyMap;  // Map of term frequency
+    typedef std::unordered_map<std::string, double> DocFeatureMap;  // Features for a given document
+
+    typedef enum FeatureNames
+    {
+        // Document length
+        dl,
+
+        // TF
+        tflognorm,
+        tfdoublenorm,
+        // IDF
+        idfdefault,
+        idfsmooth,
+        idfmax,
+        idfprob,
+        idfnorm,
+        // TF/IDF
+        tfidf,
+
+        // Okapi
+        bm25,
+        bm25plus,
+
+        // LMIR
+        abs,
+        dir,
+        jm
+    } FeatureNames;
 }
