ef1413cf4326fa820efac293693258f9febda0f6
Patrick Cox <pxcox003@odu.edu>
2019-04-16 12:16:13 -0400

    Add vector intersection function

--- include/lowletorfeats/base/utillf.hpp
+++ include/lowletorfeats/base/utillf.hpp
@@ -152,6 +152,28 @@ public:
 
 
     /**
+     * @brief Get the intersecting terms of two vectors
+     *
+     * @tparam T
+     * @param a
+     * @param b
+     * @return std::list<T>
+     */
+    template<typename T>
+    static std::vector<T> getIntersection(std::vector<T> const & a, std::vector<T> const & b)
+    {
+        std::vector<T> interLst;
+
+        std::set_intersection(
+            a.begin(), a.end(),
+            b.begin(), b.end(),
+            std::back_inserter(interLst));
+
+        return interLst;
+    }
+
+
+    /**
      * @brief Get the intersection of two unorderd_maps
      *
      * @tparam KEY_T
