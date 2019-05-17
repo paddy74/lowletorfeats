07f8afba361b97337f5beaa74d8278855ac2081f
Patrick Cox <coxpatrick74@gmail.com>
2019-05-17 17:26:16 -0400

    Update testData.hpp

--- tests/src/testData.hpp
+++ tests/src/testData.hpp
@@ -1,19 +1,28 @@
 #pragma once
 
-#include <lowletorfeats/base/stdDef.hpp>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
-std::pair<std::string, std::vector<lowletorfeats::base::StrStrMap> >
+std::pair<
+    std::string, std::vector<std::unordered_map<std::string, std::string>>>
     getTestData()
 {
-    typedef lowletorfeats::base::StrStrMap StrStrMap;
+    typedef std::unordered_map<std::string, std::string> StrStrMap;
 
-    std::string const queryString = "This query submission attempts to query.";
+    std::string const queryString =
+        "Van Helsing's face grow white and purple by turns.";
 
+    // Construct raw data
     std::vector<StrStrMap> structDocTextVector{
-        lowletorfeats::base::StrStrMap{
-            {"full",
+        StrStrMap{
+            {"id", "00000"},
+            {"rscore", "5"},
+            {"url", "https://youthere.org"},
+            {"title", "October helsing"},
+            {"author", "Van Helsing"},
+            {"anchor", "helsing@mail.com, https://hello/world.com"},
+            {"body",
              "24 October.--A whole week of waiting. Daily telegrams to "
              "Godalming, but only the same story: Not yet reported. Mina's "
              "morning and evening hypnotic answer is unvaried: lapping waves, "
@@ -43,8 +52,14 @@ std::pair<std::string, std::vector<lowletorfeats::base::StrStrMap> >
              "other. We should neither of us shrink from the task--awful "
              "though it be to contemplate. Euthanasia is an excellent and a "
              "comforting word! I am grateful to whoever invented it."}},
-        lowletorfeats::base::StrStrMap{
-            {"full",
+        StrStrMap{
+            {"id", "00001"},
+            {"rscore", "3"},
+            {"url", "https://randomhello.com"},
+            {"title", "There are such beings as vampires"},
+            {"author", "Vampire"},
+            {"anchor", "madamMina@fallen.com"},
+            {"body",
              "So we came down this road; when we meet other ways--not always "
              "were we sure that they were roads at all, for they be neglect "
              "and light snow have fallen--the horses know and they only. I "
@@ -97,15 +112,35 @@ std::pair<std::string, std::vector<lowletorfeats::base::StrStrMap> >
              "am afraid, afraid, afraid!--I am afraid of all things--even to "
              "think but I must go on my way. The stake we play for is life "
              "and death, or more than these, and we must not flinch."}},
-        lowletorfeats::base::StrStrMap{
-            {"full",
+        StrStrMap{
+            {"id", "00002"},
+            {"rscore", "1"},
+            {"url", "https://uncleanvampire.com"},
+            {"title", "Unclean vampire. Dr. Van Helsing"},
+            {"author", "Seward"},
+            {"anchor", "https://whatisyourname.com/index.html"},
+            {"body",
              "In God's name what does this mean? Harker cried out. Dr. "
              "Seward, Dr. Van Helsing, what is it? What has happened? What is "
              "wrong? Mina, dear, what is it? What does that blood mean? My "
              "God, my God! has it come to this! and, raising himself to his "
-             "knees, he beat his hands wildly together. Good God help us! "}},
-        lowletorfeats::base::StrStrMap{
-            {"full",
+             "knees, he beat his hands wildly together. Good God help us! "
+             "help her! oh, help her! With a quick movement he jumped from "
+             "bed, and began to pull on his clothes,--all the man in him "
+             "awake at the need for instant exertion. What has happened? Tell "
+             "me all about it! he cried without pausing. Dr. Van Helsing, you "
+             "love Mina, I know. Oh, do something to save her. It cannot have "
+             "gone too far yet. Guard her while I look for him! His wife, "
+             "through her terror and horror and distress, saw some sure "
+             "danger to him: instantly forgetting her own grief, she seized "
+             "hold of him and cried out: No! no! Jonathan, you must not leave "
+             "me. I have suffered enough to-night, God knows, without the "
+             "dread of his harming you. You must stay with me. Stay with "
+             "these friends who will watch over you! Her expression became "
+             "frantic as she spoke; and, he yielding to her, she pulled him "
+             "down sitting on the bed side, and clung to him fiercely. Van "
+             "Helsing and I tried to calm them both. The Professor held up "
+             "his little golden crucifix, and said with wonderful calmness: "
              "Do not fear, my dear. We are here; and whilst this is close to "
              "you no foul thing can approach. You are safe for to-night; and "
              "we must be calm and take counsel together. She shuddered and "
@@ -113,9 +148,21 @@ std::pair<std::string, std::vector<lowletorfeats::base::StrStrMap> >
              "she raised it, his white night-robe was stained with blood "
              "where her lips had touched, and where the thin open wound in "
              "her neck had sent forth drops. The instant she saw it she drew "
-             "back, with a low wail, and whispered, amidst choking sobs: "}},
-        lowletorfeats::base::StrStrMap{
-            {"full",
+             "back, with a low wail, and whispered, amidst choking sobs: "
+             "Unclean, unclean! I must touch him or kiss him no more. Oh, "
+             "that it should be that it is I who am now his worst enemy, and "
+             "whom he may have most cause to fear. To this he spoke out "
+             "resolutely: Nonsense, Mina. It is a shame to me to hear such a "
+             "word. I would not hear it of you; and I shall not hear it from "
+             "you. May God judge me by my deserts, and punish me with more "
+             "bitter suffering than even this hour, if by any act or will of "
+             "mine anything ever come between us! He put out his arms and "
+             "folded her to his breast; and for a while she lay there "
+             "sobbing. He looked at us over her bowed head, with eyes that "
+             "blinked damply above his quivering nostrils; his mouth was set "
+             "as steel. After a while her sobs became less frequent and more "
+             "faint, and then he said to me, speaking with a studied calmness "
+             "which I felt tried his nervous power to the utmost:-- And now, "
              "Dr. Seward, tell me all about it. Too well I know the broad "
              "fact; tell me all that has been. I told him exactly what had "
              "happened, and he listened with seeming impassiveness; but his "
@@ -126,8 +173,19 @@ std::pair<std::string, std::vector<lowletorfeats::base::StrStrMap> >
              "whilst the face of white set passion worked convulsively over "
              "the bowed head, the hands tenderly and lovingly stroked the "
              "ruffled hair. Just as I had finished, Quincey and Godalming "
-             "knocked at the door. They entered in obedience to our "
-             "summons. "}}};
+             "knocked at the door. They entered in obedience to our summons. "
+             "Van Helsing looked at me questioningly. I understood him to "
+             "mean if we were to take advantage of their coming to divert if "
+             "possible the thoughts of the unhappy husband and wife from each "
+             "other and from themselves; so on nodding acquiescence to him he "
+             "asked them what they had seen or done. To which Lord Godalming "
+             "answered: I could not see him anywhere in the passage, or in "
+             "any of our rooms. I looked in the study but, though he had been "
+             "there, he had gone. He had, however---- He stopped suddenly, "
+             "looking at the poor drooping figure on the bed. Van Helsing "
+             "said gravely:--"}}};
+
+    // Construct analyzed data
 
     return std::pair(queryString, structDocTextVector);
 }
