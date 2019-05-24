160bafa022bfcbfab3bc880c9c17aa859d72c9d8
Patrick Cox <coxpatrick74@gmail.com>
2019-05-24 15:16:25 -0400

    More test coverage

--- tests/src/test_FC.cpp
+++ tests/src/test_FC.cpp
@@ -4,12 +4,39 @@
 
 int main()
 {
+    // Get test data
     auto const testData = getTestData();
     auto const queryStr = testData.first;
     auto const structDocMap = testData.second;
 
-    auto fc = lowletorfeats::FeatureCollector(structDocMap, queryStr);
+    // Test constructors
+    lowletorfeats::FeatureCollector fc;
+    fc = lowletorfeats::FeatureCollector();
+    fc = lowletorfeats::FeatureCollector(structDocMap, queryStr);
+    //
+    //
+    //
+
+    // Test public methods
+    fc.toString();
+    fc.getFeatureString();
+
     fc.collectPresetFeatures();
+    fc.reCollectFeatures();
+    fc.collectFeatures(lowletorfeats::base::FeatureKey("tfidf.tfidf.full"));
+
+    // Getter methods
+    fc.getNumDocs();
+    fc.getNumFeatures();
+    fc.getDocVect();
+    fc.getFeatureVects();
+
+    // Setter methods
+    std::unordered_map<std::string, float> newSectionWeights = {
+        {"full", 0.3},   {"title", 1},    {"body", 0.4},
+        {"author", 0.9}, {"anchor", 0.5}, {"url", 0.7}};
+    fc.setSectionWeights(newSectionWeights);
+    // fc.setAnalyzerFunction(lowletorfeats::FeatureCollector::analyzerFun);
 
     return 0;
 }
